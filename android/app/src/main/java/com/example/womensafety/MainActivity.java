65a33e0a4bcaa5edaad9b06c3d556cc3e34a25a0
Kirti Bhushan <kirti18bhushan@gmail.com>
2025-03-19 13:18:03 +0530

    Initial commit

---
+++ android/app/src/main/java/com/example/womensafety/MainActivity.java
@@ -0,0 +1,5 @@
+package com.example.womensafety;
+
+import com.getcapacitor.BridgeActivity;
+
+public class MainActivity extends BridgeActivity {}
---
+++ capacitor.config.ts
@@ -0,0 +1,10 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'com.example.womensafety',
+  appName: 'Women Safety Real-Time Tracking and Emergency',
+  webDir: 'www',
+  bundledWebRuntime: false
+};
+
+export default config;
